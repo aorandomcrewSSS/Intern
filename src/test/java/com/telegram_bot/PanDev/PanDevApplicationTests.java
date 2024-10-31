2e73f737ea49eaf0655090cd633ee011ea40f8f0
aorandomcrewSSS <danbogomolovhr@gmai.com>
2024-10-31 17:49:35 +0300

    Initial commit

---
+++ src/test/java/com/telegram_bot/PanDev/PanDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegram_bot.PanDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
