89c51243b1276f68f5aec58725504d3f7c78c6b2
aorandomcrewSSS <danbogomolovhr@gmai.com>
2024-11-01 06:08:38 +0300

    Небольшой рефакторинг

--- src/main/java/com/telegram_bot/PanDev/serivce/TelegramBot.java
+++ src/main/java/com/telegram_bot/PanDev/serivce/TelegramBot.java
@@ -2,7 +2,6 @@ package com.telegram_bot.PanDev.serivce;
 
 import com.telegram_bot.PanDev.config.TelegramBotConfig;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -40,26 +39,32 @@ public class TelegramBot extends TelegramLongPollingBot {
             long chatId = update.getMessage().getChatId();
             String[] parts = messageText.split(" ");
 
-            if (messageText.equals("/start")) {
-                startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-            } else if (messageText.equals("/viewTree")) {
-                sendMessage(chatId, categoryService.formatTree());
-            } else if (messageText.startsWith("/addElement")) {
-                if (parts.length == 3) {
-                    sendMessage(chatId, categoryService.addElement(parts[1], parts[2]));
-                } else {
-                    sendMessage(chatId, "Неверный формат. Используйте /addElement <родитель> <название>");
-                }
-            } else if (messageText.startsWith("/removeElement")) {
-                if (parts.length == 2) {
-                    sendMessage(chatId, categoryService.removeElement(parts[1]));
-                } else {
-                    sendMessage(chatId, "Неверный формат. Используйте /removeElement <название>");
-                }
-            } else if (messageText.equals("/help")) {
-                sendMessage(chatId, help());
-            } else {
-                sendMessage(chatId, "Извините, неизвестная команда.");
+            switch (parts[0]) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/viewTree":
+                    sendMessage(chatId, categoryService.formatTree());
+                    break;
+                case "/addElement":
+                    if (parts.length == 3) {
+                        sendMessage(chatId, categoryService.addElement(parts[1], parts[2]));
+                    } else {
+                        sendMessage(chatId, "Неверный формат. Используйте /addElement <родитель> <название>");
+                    }
+                    break;
+                case "/removeElement":
+                    if (parts.length == 2) {
+                        sendMessage(chatId, categoryService.removeElement(parts[1]));
+                    } else {
+                        sendMessage(chatId, "Неверный формат. Используйте /removeElement <название>");
+                    }
+                    break;
+                case "/help":
+                    sendMessage(chatId, help());
+                    break;
+                default:
+                    sendMessage(chatId, "Sorry, unknown command");
             }
         }
 
@@ -74,7 +79,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                 """;
     }
     private void startCommandReceived(long chatId, String name) {
-        String answer = "Hi, " + name + "! nice to meet you!";
+        String answer = "Привет! " + name + " используй следующие команды, чтобы построить дерево!" + "\n" + help();
         log.info("replied to user " + answer);
         sendMessage(chatId, answer);
     }
@@ -83,7 +88,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
-
+        log.info("replied to user " + message);
         try {
             execute(message);
         } catch (TelegramApiException e){
